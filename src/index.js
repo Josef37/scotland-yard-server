4deb7fea07387a9aa63e1089d00eaed9150041a0
Josef Wittmann <josefwittmann42@gmail.com>
2019-12-06 08:29:05 +0100

    Added map submission to server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const GameScheduler = require("./GameScheduler");
 const io = require("socket.io")(80);
 
@@ -8,6 +9,26 @@ io.on("connection", socket => {
   socket.on("set name", name => {
     gameScheduler.takeNewPlayer(name, socket);
   });
+
+  socket.on(
+    "submit gameboard",
+    (stations, connections, startingPositions, confirmSuccess) => {
+      const map = { stations, connections, startingPositions };
+      fs.writeFile(
+        __dirname + `/../maps/map-${Date.now()}`,
+        JSON.stringify(map),
+        "utf8",
+        err => {
+          if (err) {
+            console.log(err);
+            confirmSuccess(false);
+          } else {
+            confirmSuccess(true);
+          }
+        }
+      );
+    }
+  );
 
   socket.on("disconnect", () => {
     gameScheduler.leaveLobby(socket);
